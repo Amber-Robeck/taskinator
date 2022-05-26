2ae09d4f3940f54a80255393ea5481306fc31f25
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-05-25 20:20:48 -0500

    Created functions in script.js to take in user input and refactored function to  create a list item

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,36 @@
-var addTaskButton = document.querySelector(".btn");
+var userForm = document.querySelector("#task-form");
 var tasksToDo = document.querySelector("#tasks-to-do");
 
+//Function to collect user input and store in object format
+var createTaskHandler = function (event) {
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-var createTaskHandler = function () {
+    var formInputValues = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createListItem(formInputValues);
+};
+
+//Function to create list item with data passed in arguments
+var createListItem = function (data) {
+    // create list item
     var taskItem = document.createElement("li");
     taskItem.className = "task-item";
-    taskItem.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfo = document.createElement("div");
+    taskInfo.className = "task-info";
+
+    // add HTML content to div
+    taskInfo.innerHTML = "<h3 class='task-name'>" + data.name + "</h3><span class='task-type'>" + data.value + "</span>";
+    taskItem.appendChild(taskInfo);
+
+    // add entire list item to list
     tasksToDo.appendChild(taskItem);
 };
-addTaskButton.addEventListener("click", createTaskHandler);
+
+userForm.addEventListener("submit", createTaskHandler);
